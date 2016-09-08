d3210d729871403b771a0cdcb9d64997c101e0ef
Shaurya Sharma <shaur141@gmail.com>
2016-09-08 20:32:28 +0200

    wip datastructure

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <utility>
 #include <unordered_map>
 #include <deque>
+#include <list>
 #include <map>
 
 namespace Pricer
@@ -25,26 +26,26 @@ namespace Pricer
     };
 
 	template<typename CMP_FUNC>
-    class OrderBook2
+    class OrderBook
     {
     public:
         using quantity_t = uint32_t;
         using price_t = uint64_t;
-        using price_levels_t = std::deque<quantity_t>;
+        using price_levels_t = std::list<quantity_t>;
 
-        using order_book_t = std::map<price_t, price_levels_t>;
-        using order_book_it_t = order_book_t::iterator;
+        using order_book_t = std::map<price_t, price_levels_t, CMP_FUNC>;
+        typedef typename order_book_t::iterator order_book_it_t;
 
-        using order_id_t = std::pair<order_book_it_t, size_t>;
+		using order_ptr = std::list<uint32_t>::iterator;
+        using order_id_t = std::pair<order_book_it_t, order_ptr>;
         using order_book_id_t = std::unordered_map<char, order_id_t>;
-        //using price_level_map_t = std::unordered_map<price_t, order_book_it_t>;
     private:
         uint64_t _total_orders;
 
         order_book_t _orders;
         order_book_id_t _order_ids;
-        //price_level_map_t _price_levels;
 
+	public:
         void add_order(const Pricer::Order &order)
         {
             const bool found_order = _order_ids.end() != _order_ids.find(order.id);
@@ -58,16 +59,17 @@ namespace Pricer
 			const auto price_level_found_it = _orders.find(price_as_int);
             if( _orders.end() != price_level_found_it )
             {
-				std::deque<uint32_t> &levels = price_level_found_it->second;
+				std::list<uint32_t> &levels = price_level_found_it->second;
 				levels.push_back(order.size);
-				_order_ids[order.id] = std::make_pair(price_level_found_it, levels.size() - 1);
+				_order_ids[order.id] = std::make_pair(price_level_found_it, --levels.end());
             }
 			else
 			{
-				const std::pair<order_book_it_t, bool> insert_result = _orders.insert(price_as_int);
-				std::deque<uint32_t> &levels = insert_result.first->second;
+				const auto key_value = std::make_pair(price_as_int, std::list<uint32_t>{order.size});
+				const std::pair<order_book_it_t, bool> insert_result = _orders.insert(key_value);
+				std::list<uint32_t> &levels = insert_result.first->second;
 				levels.push_back(order.size);
-				_order_ids[order.id] = std::make_pair(insert_result.first, 0);
+				_order_ids[order.id] = std::make_pair(insert_result.first, levels.begin());
 			}
 			_total_orders += order.size;
         }
@@ -75,133 +77,97 @@ namespace Pricer
         void reduce_order(const Pricer::Order &order)
         {
 			auto found_it = _order_ids.find(order.id);
+			//std::pair<order_book_it_t, size_t> p = found_it->second;
+			//order_book_it_t tt = p.first;
+			//uint64_t xx =tt->first;
+			//std::deque<uint32_t> &dq = tt->second;
 			const bool found = found_it != _order_ids.end();
 			if (!found)
 				return;
 
-			std::deque<uint32_t> &levels = found_it->second;			
-			size_t index = found_it->second.second;
-			uint32_t previous_size = levels[index];
+			std::list<uint32_t> &levels = found_it->second.first->second;
+			auto it = found_it->second.second;
+			uint32_t previous_size = *it;
 			if (order.size >= previous_size)
 			{
-				levels.erase(index);
+				levels.erase(it);
 			}
 			else
 			{
-				levels[index] -= order.size;
+				*it -= order.size;
 			}
         }
+
+        const order_book_t &get_order_book() const noexcept { return _orders; }
+		const order_book_id_t &get_order_id_book() const noexcept { return _order_ids; }
     };
-
-	class OrderBook1
-	{
-	public:
-		using quantity_t = uint32_t;
-		using price_t = double;
-
-		using order_book_t = std::map <price_t, quantity_t>;
-		using order_book_it = order_book_t::iterator;
-		using order_book_id_t = std::unordered_map < char, order_book_it >;
-
-	private:
-		uint64_t _total_asks;
-		uint64_t _total_bids;
-
-		order_book_t _bid_orders;
-		order_book_id_t _bid_order_ids;
-
-		order_book_t _ask_orders;
-		order_book_id_t _ask_order_ids;
-
-		void add_order(const Pricer::Order &order)
-		{
-			bool found_bid = _bid_order_ids.end() != _bid_order_ids.find(order.id);
-			bool found_ask = _ask_order_ids.end() != _ask_order_ids.find(order.id);
-			if (found_bid || found_ask)
-				throw std::exception("add order duplicate id");
-
-			const auto &add_order_info = boost::get<Pricer::AddOrder>(order._order);
-
-			const order_book_t::value_type price_quantity = std::make_pair(add_order_info.limit_price, order.size);
-
-			if (add_order_info.order_type == Pricer::ADD_ORDER_TYPE::ASK)
-			{
-				auto &order_book = _ask_orders;
-				auto &order_id_book = _ask_order_ids;
-				_total_asks += order.size;
-				const std::pair<order_book_it, bool> insert_result = order_book.insert(price_quantity);
-				order_id_book[order.id] = insert_result.first;
-			}
-			else
-			{
-				auto &order_book = _bid_orders;
-				auto &order_id_book = _bid_order_ids;
-				_total_bids += order.size;
-				const std::pair<order_book_it, bool> insert_result = order_book.insert(price_quantity);
-				order_id_book[order.id] = insert_result.first;
-			}
-		}
-
-		void reduce_order(const Pricer::Order &order)
-		{
-			bool found_bid = _bid_order_ids.end() != _bid_order_ids.find(order.id);
-			bool found_ask = _ask_order_ids.end() != _ask_order_ids.find(order.id);
-			if (!found_bid || !found_ask)
-				throw std::exception("did not find order id");
-
-			auto &order_book = _bid_orders;
-			auto &order_id_book = _bid_order_ids;
-            if( found_ask )
-            {
-                order_book = _ask_orders;
-                order_id_book = _ask_order_ids;
-            }
-
-            order_book_it it = order_id_book[order.id];
-			quantity_t order_quantity = it->second;
-            if( order.size >= order_quantity)
-            {
-                order_book.erase( it );
-                order_id_book.erase( order.id );
-            }
-            else
-            {
-                it->second -= order.size;
-            }
-		}
-
-	public:
-		OrderBook1() :_total_asks(0), _total_bids(0) {}
-
-		void process_order(const Pricer::Order &order)
-		{
-			switch (order.type)
-			{
-			case ORDER_TYPE::ADD:
-				add_order(order);
-				break;
-			case ORDER_TYPE::REDUCE:
-				reduce_order(order);
-				break;
-			}
-		}
-
-		const order_book_t &get_bid_orders() const noexcept { return _bid_orders; };
-		const order_book_t &get_ask_orders() const noexcept { return _ask_orders; };
-	};
 
 	class PricingEngine
 	{
+    public:
+        using BidOrderBook = OrderBook<std::greater<uint64_t>>;
+        using AskOrderBook = OrderBook<std::less<uint64_t>>;
+
 	private:
-		OrderBook1 order_book;
-		long int _target_size;
+        long int _target_size;
+
+        BidOrderBook _bid_order_book;
+        AskOrderBook _ask_order_book;
+
+		template<typename T>
+		bool id_in_order_book(const T& order_ids, char id) const noexcept
+		{
+			return order_ids.end() != order_ids.find(id);
+		}
+
+		void process_add_order(const Pricer::Order& order)
+		{
+			const auto& add_order = boost::get<AddOrder>(order._order);
+			switch (add_order.order_type)
+			{
+			case Pricer::ADD_ORDER_TYPE::ASK:
+				_ask_order_book.add_order(order);
+				break;
+			case Pricer::ADD_ORDER_TYPE::BID:
+				_bid_order_book.add_order(order);
+				break;
+			}
+		}
+
+		void process_reduce_order(const Pricer::Order &order)
+		{
+            char id = order.id;
+            const auto& bid_ids = _bid_order_book.get_order_id_book();
+            const auto& ask_ids = _ask_order_book.get_order_id_book();
+            if( id_in_order_book( bid_ids, id ) )
+            {
+                _bid_order_book.reduce_order( order );
+            }
+            else if( id_in_order_book( ask_ids, id ) )
+            {
+                _ask_order_book.reduce_order( order );
+            }
+            else
+            {
+                //TODO: throw exception or return error
+            }
+		}
+
 	public:
 		PricingEngine(long int target_size) : _target_size(target_size){}
 
 		void init(){}
 		void process(const Pricer::Order &order)
 		{
-			order_book.process_order(order);
+            switch( order.type )
+            {
+			case Pricer::ORDER_TYPE::ADD:
+				process_add_order(order);
+                break;
+			case Pricer::ORDER_TYPE::REDUCE:
+				process_reduce_order(order);
+				break;
+            }
 		}
 	};
 }
@@ -239,8 +205,11 @@ static void signal_handler(int sig)
 	throw cancel_exception();
 }
 
+
+
 int main(int argc, char *argv[])
 {
+
 	signal(SIGINT, signal_handler);
 	try
 	{
